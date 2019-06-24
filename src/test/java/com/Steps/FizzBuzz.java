d2fc72597100dd75ea10eec3b47a2f227b98f0c6
Halima Abedin <halimaabedin@gmail.com>
2019-06-24 13:41:00 -0400

    fizzbuzz

---
+++ src/test/java/com/Steps/FizzBuzz.java
@@ -0,0 +1,28 @@
+package com.Steps;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+
+		/*
+		 * for (int i = 1; i <= 100; i++) { if (i % 3 == 0 && i % 5 != 0) {
+		 * System.out.println("fizz"); } else if (i % 3 != 0 && i % 5 == 0) {
+		 * System.out.println("buzz"); } else if (i % 3 == 0 && i % 5 == 0) {
+		 * System.out.println("fizzbuzz"); } else { System.out.println(i); } }
+		 */
+
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("fizzbuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("buzz");
+			} else {
+				System.out.println(i);
+			}
+
+		}
+
+	}
+}
